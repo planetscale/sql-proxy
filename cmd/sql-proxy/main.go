ceb4cb2dd6af5e006184e574405c3ec43b270d17
Fatih Arslan <ftharsln@gmail.com>
2021-01-07 14:44:10 +0300

    Add working example of using TLS certs

--- cmd/sql-proxy/main.go
+++ cmd/sql-proxy/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"database/sql"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/go-sql-driver/mysql"
@@ -23,12 +26,44 @@ func main() {
 func realMain() error {
 	user := flag.String("user", "root", "MySQL user")
 	password := flag.String("password", "", "MySQL password")
-	addr := flag.String("addr", "", "MySQL network address")
+	addr := flag.String("addr", "127.0.0.1", "MySQL network address")
 	dbname := flag.String("db", "", "MySQL Database name")
+
+	caPath := flag.String("ca", "testcerts/ca.pem", "MySQL CA Cert path")
+	clientCertPath := flag.String("key", "testcerts/client-cert.pem", "MySQL Client Cert path")
+	clientKeyPath := flag.String("cert", "testcerts/client-key.pem", "MySQL Client Key path")
 
 	flag.Parse()
 
-	fmt.Printf("*addr = %+v\n", *addr)
+	rootCertPool := x509.NewCertPool()
+	pem, err := ioutil.ReadFile(*caPath)
+	if err != nil {
+		return err
+	}
+
+	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+		log.Fatal("failed to append PEM.")
+	}
+
+	certs, err := tls.LoadX509KeyPair(*clientCertPath, *clientKeyPath)
+	if err != nil {
+		return err
+	}
+
+	if err := mysql.RegisterTLSConfig("custom", &tls.Config{
+		RootCAs:            rootCertPool,
+		Certificates:       []tls.Certificate{certs},
+		InsecureSkipVerify: true, // TODO(fatih): make sure if we can disable this
+		VerifyConnection: func(st tls.ConnectionState) error {
+			for _, p := range st.PeerCertificates {
+				fmt.Printf("p.Issuer = %+v\n", p.Issuer)
+				fmt.Printf("p.Subject = %+v\n", p.Subject)
+			}
+			return nil
+		},
+	}); err != nil {
+		return err
+	}
 
 	cfg := mysql.NewConfig()
 	cfg.User = *user
@@ -36,14 +71,15 @@ func realMain() error {
 	cfg.Net = "tcp"
 	cfg.Addr = *addr
 	cfg.DBName = *dbname
+	cfg.TLSConfig = "custom" // should match mysql.RegisterTLSConfig()
 
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
-		return err
+		return fmt.Errorf("could not open SQL: %s", err)
 	}
 	err = db.Ping() // check that connection is working
 	if err != nil {
-		return err
+		return fmt.Errorf("could not ping: %s", err)
 	}
 
 	rows, err := db.Query("SELECT * FROM cities")
