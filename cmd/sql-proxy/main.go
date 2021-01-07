13ff1e9b45fc150b2ac23e741c68e59b09de2cb2
Fatih Arslan <ftharsln@gmail.com>
2021-01-07 22:27:57 +0300

    Initial local test proxy

--- cmd/sql-proxy/main.go
+++ cmd/sql-proxy/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"database/sql"
+	"encoding/pem"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/go-sql-driver/mysql"
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/logging"
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/proxy"
 )
 
 type City struct {
@@ -24,9 +31,9 @@ func main() {
 }
 
 func realMain() error {
-	user := flag.String("user", "root", "MySQL user")
-	password := flag.String("password", "", "MySQL password")
-	addr := flag.String("addr", "127.0.0.1", "MySQL network address")
+	// user := flag.String("user", "root", "MySQL user")
+	// password := flag.String("password", "", "MySQL password")
+	remoteAddr := flag.String("remote-addr", "127.0.0.1", "MySQL network address")
 	dbname := flag.String("db", "", "MySQL Database name")
 
 	caPath := flag.String("ca", "testcerts/ca.pem", "MySQL CA Cert path")
@@ -35,72 +42,133 @@ func realMain() error {
 
 	flag.Parse()
 
-	rootCertPool := x509.NewCertPool()
-	pem, err := ioutil.ReadFile(*caPath)
+	certSource, err := newLocalCertSource(*remoteAddr, *dbname, *caPath, *clientCertPath, *clientKeyPath)
 	if err != nil {
 		return err
 	}
 
-	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
-		log.Fatal("failed to append PEM.")
+	proxyClient := &proxy.Client{
+		Port:           3306, // remote DB port
+		MaxConnections: 0,    // no limit
+		Conns:          proxy.NewConnSet(),
+		Certs:          certSource,
 	}
 
-	certs, err := tls.LoadX509KeyPair(*clientCertPath, *clientKeyPath)
-	if err != nil {
-		return err
-	}
-
-	if err := mysql.RegisterTLSConfig("custom", &tls.Config{
-		RootCAs:            rootCertPool,
-		Certificates:       []tls.Certificate{certs},
-		InsecureSkipVerify: true, // TODO(fatih): make sure if we can disable this
-		VerifyConnection: func(st tls.ConnectionState) error {
-			for _, p := range st.PeerCertificates {
-				fmt.Printf("p.Issuer = %+v\n", p.Issuer)
-				fmt.Printf("p.Subject = %+v\n", p.Subject)
-			}
-			return nil
-		},
-	}); err != nil {
-		return err
-	}
-
-	cfg := mysql.NewConfig()
-	cfg.User = *user
-	cfg.Passwd = *password
-	cfg.Net = "tcp"
-	cfg.Addr = *addr
-	cfg.DBName = *dbname
-	cfg.TLSConfig = "custom" // should match mysql.RegisterTLSConfig()
-
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		return fmt.Errorf("could not open SQL: %s", err)
-	}
-	err = db.Ping() // check that connection is working
-	if err != nil {
-		return fmt.Errorf("could not ping: %s", err)
-	}
-
-	rows, err := db.Query("SELECT * FROM cities")
-	defer rows.Close()
-	if err != nil {
-		return err
-	}
-
-	for rows.Next() {
-		var city City
-		err := rows.Scan(&city.ID, &city.Name)
+	addr := "127.0.0.1"
+	connSrc := make(chan proxy.Conn, 1)
+	go func() {
+		l, err := net.Listen("tcp", addr+":3307")
 		if err != nil {
-			return err
+			log.Println("error net.Listen: %s", err)
+			return
 		}
 
-		fmt.Printf("%v\n", city)
-	}
+		fmt.Printf("l = %+v\n", l.Addr().String())
 
-	if err := rows.Err(); err != nil {
-		return err
-	}
+		go func() {
+			for {
+				start := time.Now()
+				c, err := l.Accept()
+				if err != nil {
+					log.Printf("error in accept for on %v: %v", addr, err)
+					if nerr, ok := err.(net.Error); ok && nerr.Temporary() {
+						d := 10*time.Millisecond - time.Since(start)
+						if d > 0 {
+							time.Sleep(d)
+						}
+						continue
+					}
+					l.Close()
+					return
+				}
 
+				log.Printf("new connection for %q", addr)
+
+				switch clientConn := c.(type) {
+				case *net.TCPConn:
+					clientConn.SetKeepAlive(true)
+					clientConn.SetKeepAlivePeriod(1 * time.Minute)
+
+				}
+				connSrc <- proxy.Conn{*dbname, c}
+			}
+		}()
+
+		logging.Infof("listening on %s for %s", addr, *dbname)
+
+	}()
+
+	// TODO(fatih): replace with signal.NotifyContext once Go 1.16 is released
+	// https://go-review.googlesource.com/c/go/+/219640
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
+	termTimeout := time.Second * 1
+	go func() {
+		<-signals
+		log.Printf("Received TERM signal. Waiting up to %s before terminating.", termTimeout)
+
+		err := proxyClient.Shutdown(termTimeout)
+		if err == nil {
+			os.Exit(0)
+		}
+		log.Printf("Error during SIGTERM shutdown: %v", err)
+		os.Exit(2)
+	}()
+
+	log.Println("Ready for new connections")
+
+	proxyClient.Run(connSrc)
 	return nil
+}
+
+func newLocalCertSource(addr, dbname, caPath, certPath, keyPath string) (*localCertSource, error) {
+	pem, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	caCert, err := parseCert(pem)
+	if err != nil {
+		return nil, err
+	}
+
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+	cert.Leaf = caCert
+
+	return &localCertSource{
+		cert:   cert,
+		caCert: caCert,
+		name:   dbname,
+		addr:   addr,
+	}, nil
+
+}
+
+type localCertSource struct {
+	cert   tls.Certificate
+	caCert *x509.Certificate
+
+	name string
+	addr string
+}
+
+func (c *localCertSource) Local(instance string) (tls.Certificate, error) {
+	fmt.Println("local is called for instance:", instance)
+	return c.cert, nil
+}
+
+func (c *localCertSource) Remote(instance string) (cert *x509.Certificate, addr, name, version string, err error) {
+	fmt.Println("remote is called for instance:", instance)
+	return c.caCert, c.addr, c.name, "", nil
+}
+
+func parseCert(pemCert []byte) (*x509.Certificate, error) {
+	bl, _ := pem.Decode(pemCert)
+	if bl == nil {
+		return nil, errors.New("invalid PEM: " + string(pemCert))
+	}
+	return x509.ParseCertificate(bl.Bytes)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/planetscale/sql-proxy
 
 go 1.15
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/GoogleCloudPlatform/cloudsql-proxy v1.19.1
+	github.com/go-sql-driver/mysql v1.5.0
+)
